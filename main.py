554ef86bf0c9e12f6e9793d58dc01e10fe59977a
xnrrrrrr <xnrrrrrr@gmail.com>
2024-03-12 15:40:00 -0400

    Your commit message

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ def gen_pins():
     # Generates the pins
     pins = {str(n).zfill(4) for n in range(0, 10000)}
     return pins
-
+#steve
 
 def save_pins(pins, filename):
     # Saves the pins to a text file
@@ -10,7 +10,7 @@ def save_pins(pins, filename):
         for pin in pins:
             file.write(f'{pin}\n')
 
-
+#steve
 def main():
     # Runs the script
     filename = input("Enter the filename for generated PINs: ")
